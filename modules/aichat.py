bd8ddf0a65cd6090d8c463f9fcdbcb0b623f3f97
Shota Minegishi <smngs@smngs.io>
2023-03-02 21:52:30 +0900

    [update] use ChatGPT API (instead of GPT-3) in `/chat` command

--- modules/aichat.py
+++ modules/aichat.py
@@ -11,24 +11,22 @@ from config import DISCORD_SERVER_KEY, OPENAI_API_KEY
 
 guild = discord.Object(id=DISCORD_SERVER_KEY)
 
-async def get_chat(prompt: str):
+async def get_chatapi_response(messages):
     headers = {
         "Content-Type": "application/json",
         "Authorization": "Bearer " + str(OPENAI_API_KEY)
     }
 
     data_json = {
-        "model": "text-davinci-003",
-        "prompt": prompt,
-        "max_tokens": 1024,
-        "temperature": 0.5,
+        "model": "gpt-3.5-turbo",
+        "messages": messages
     }
 
     async with aiohttp.ClientSession("https://api.openai.com", json_serialize=ujson.dumps) as session:
-        async with session.post("/v1/completions", headers=headers, json=data_json) as r:
+        async with session.post("/v1/chat/completions", headers=headers, json=data_json) as r:
             if r.status == 200:
                 json_body = await r.json()
-                return json_body["choices"][0]["text"]
+                return json_body["choices"][0]["message"]["content"]
 
 def generate_embed(prompt: str, user: discord.User) -> discord.Embed:
     embed = discord.Embed(
@@ -45,16 +43,21 @@ class Chat(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(name="chat", description="GPT-3 とおしゃべりします．")
+    @app_commands.command(name="chat", description="ChatGPT とおしゃべりします．")
     @app_commands.guilds(guild)
     @discord.app_commands.describe(
-        prompt="GPT-3 に話しかける内容です．"
+        prompt="ChatGPT に話しかける内容です．"
     )
     async def send_chat(self, ctx: discord.Interaction, prompt: str):
         await ctx.response.defer()
         await ctx.followup.send(embed=generate_embed(prompt, ctx.user))
         async with ctx.channel.typing():
-            answer = await get_chat(prompt)
+            answer = await get_chatapi_response(
+                [{
+                    "role": "user",
+                    "content": prompt
+                }]
+            )
         await ctx.followup.send(answer)
 
 async def setup(bot: commands.Bot) -> None:
